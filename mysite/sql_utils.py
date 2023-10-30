01c06e802b1495aee21fc801b05664fe49cb7540
Yong Choi <ychoi_kr@naver.com>
2023-10-30 21:10:39 +0900

    Prevent adding _ean and _url columns for DISTINCT queries.

--- mysite/sql_utils.py
+++ mysite/sql_utils.py
@@ -23,6 +23,10 @@ def add_ean_and_url_columns(sql_query):
     Add the 'ean' and 'url' columns to the SQL SELECT statement if they're not already present. 
     The resulting columns will be aliased as '_ean' and '_url'.
     """
+    # Check if the query contains 'DISTINCT', if so just pass
+    if re.search(r"SELECT\s+DISTINCT", sql_query, re.IGNORECASE):
+        return sql_query
+    
     # Check if the query is a SELECT statement followed by a "FROM BOOKS" clause
     if re.search(r"FROM\s+BOOKS", sql_query, re.IGNORECASE):
         # If the query is selecting all columns (*), just pass
