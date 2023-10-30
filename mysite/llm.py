b8fa22c65f689ad888d1da60c9e7f1c5d5a9b05c
Yong Choi <ychoi_kr@naver.com>
2023-10-30 12:46:59 +0900

    Add SQL validation and correction for SQLite

--- mysite/llm.py
+++ mysite/llm.py
@@ -1,5 +1,6 @@
 import openai
 import os
+import re
 
 openai.api_key_path = "/home/askwikibook/settings/OPENAI_API_KEY"
 if not os.path.exists(openai.api_key_path):
@@ -27,7 +28,26 @@ def generateSQL(schemas, natural_query, dialect="SQLite"):
     response_str = completion.choices[0].message.content
     #messages.append({"role": "assistant", "content": response_str})
 
-    return response_str
+    return validate_and_correct_sql(response_str, dialect)
+
+
+def validate_and_correct_sql(sql, dialect="SQLite"):
+    if dialect == "SQLite":
+        # SQLite에서 INFORMATION_SCHEMA.COLUMNS 또는 information_schema.columns가 발견되면 PRAGMA로 변경
+        if re.search(r"INFORMATION_SCHEMA\.COLUMNS", sql, re.IGNORECASE):
+            table_name_match = re.search(r"TABLE_NAME\s*=\s*'(\w+)'", sql, re.IGNORECASE)
+            if table_name_match:
+                table_name = table_name_match.group(1)
+                return f"PRAGMA table_info({table_name});"
+        
+        # SELECT COLUMN_NAME FROM pragma_table_info('table_name') 패턴 감지
+        pragma_match = re.search(r"SELECT COLUMN_NAME FROM pragma_table_info\('(\w+)'\);", sql)
+        if pragma_match:
+            table_name = pragma_match.group(1)
+            return f"PRAGMA table_info({table_name});"
+    
+    return sql
+
 
 def natural_chat(message):
     messages = [
