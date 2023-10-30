c1a17ee659f109dbc680d3673fb0f63bba0dd0f9
Yong Choi <ychoi_kr@naver.com>
2023-10-31 01:39:22 +0900

    Enhance SQL generation with extended user prompts and hints
    - Refactor system prompt to a separate variable for better readability.
    - Provide additional user prompts based on specific conditions detected in the natural query, such as keywords related to book character, product/service name, book publication, and more.
    - Offer hints for nuances in the Korean language and considerations for specific keywords like Java and C++.
    - Suggest looking at different database fields based on detected terms in the query.

--- mysite/llm.py
+++ mysite/llm.py
@@ -9,17 +9,45 @@ if not os.path.exists(openai.api_key_path):
 
 
 def generateSQL(schemas, natural_query, dialect="SQLite"):
+    system_prompt = "You interpret a query in natural language into SQL which retrieve data from 위키북스's database. "
+    system_prompt += "The company publishes books on IT such like programming, AI, OS. "
     messages = [
-        {"role": "system", "content": "You interpret a query in natural language into SQL which retrieve data from 위키북스's database. The company publishes books on IT such like programming, AI, OS."},
+        {"role": "system", "content": system_prompt},
     ]
-    prompt = "Table(s) in " + dialect + " database:\n"
-    for table, schema in schemas.items():
-        prompt += table + ':' + schema + '\n'
-    prompt += "\n\nMake SQL query doing:\n"
-    prompt += natural_query
-    prompt += "\n\nIf search keyword has two or more words, consider to split it into multiple keywords.\n\nSQL:"
 
-    messages.append({"role": "user", "content": prompt})
+    user_prompt = "Table(s) in " + dialect + " database:\n"
+    for table, schema in schemas.items():
+        user_prompt += table + ':' + schema + '\n'
+    user_prompt += "\n\nMake SQL query doing:\n"
+    user_prompt += natural_query
+    user_prompt += "\n\nHints:\n\n"
+    #user_prompt += "Before create SQL, think about what a query in Korean would mean in English.\n"
+    user_prompt += "If search keyword has two or more words, consider to split it into multiple keywords.\n"
+    user_prompt += "Words that describe the character of a book may not be keywords.\n"
+    user_prompt += "A good place to look for a product or service name is in the title or book_intro.\n"
+
+    if re.search(r"\b신간\b", natural_query):
+        user_prompt += 'In Korean, the word "신간" means "new book", so it probably won\'t be included in the title or intro_book.\n'
+
+    else:
+        if not any(map(lambda x: x in natural_query, ["구간", "부터", "순서", "순으로", "옛날", "오래된", "정렬"])):
+            user_prompt += "Unless otherwise specified, it sorts in reverse order of pubdate.\n"
+    
+        if not any(map(lambda x: x in natural_query, [" 다 ", "다알려줘", "다찾아", "모두", "모든", "싹다", "싹 다", "전부", "하나도 빼놓지 말고"])):
+            user_prompt += "Unless otherwise specified, it limits to 10.\n"
+
+    if "자바" in natural_query and not "자바스크립트" in natural_query:
+        user_prompt += "When using Java as a keyword, it's a good idea to exclude JavaScript.\n"
+
+    if "c++" in natural_query:
+        user_prompt += 'You can also get good results by searching for "C++" as well as "c++".\n'
+
+    if any(map(lambda x: x in natural_query, ["교수", "근무", "대학교", "박사", "석사", "재직", "졸업", "출신"])) or re.search(r"대\b", natural_query):
+        user_prompt += "Organization name can also be found in intro_author.\n"
+
+    user_prompt += "\nSQL:"
+    print(user_prompt)
+    messages.append({"role": "user", "content": user_prompt})
 
     completion = openai.ChatCompletion.create(
         model="gpt-3.5-turbo",
