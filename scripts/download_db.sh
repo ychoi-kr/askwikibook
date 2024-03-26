2a2fe48294e756a0bb651aa2e7a86e5d7ea30759
Yong Choi <ychoi.wikibook@gmail.com>
2024-03-26 03:56:34 +0000

    Update download_db.sh

--- scripts/download_db.sh
+++ scripts/download_db.sh
@@ -1,4 +1,6 @@
 #!/bin/sh
 
-wget --no-check-certificate 'https://drive.google.com/uc?export=download&id=1DEjOVOhGE4rbsi7hpbNZuOkIQo2lQtSf' -O ../databases/books.db
+file_id="1XUJj_daYch9DvMlnw7j9IkF_G1Vaz2zU"
+
+wget --no-check-certificate "https://docs.google.com/uc?export=download&id=${file_id}" -O ../databases/books.db
 
